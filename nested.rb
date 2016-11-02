5434480ca373bda50edbf4505ecbc349e545ab34
Keegan Leitz <kjleitz@gmail.com>
2016-11-02 11:24:39 -0400

    fix syntactic typo in the adding_matz instructions
    Fixes https://github.com/learn-co-curriculum/simple-nesting/issues/18
    
    Changed `:languages => ["LISP, C"]` to `:languages => ["LISP", "C"]`

--- nested.rb
+++ nested.rb
@@ -64,7 +64,7 @@ def adding_matz
 # add the following information to the top level of the programmer hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
+#    :languages => ["LISP", "C"]
 # }
 
   programmer_hash = 
