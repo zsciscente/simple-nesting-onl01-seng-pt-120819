c29373c3442989c4fcdbaf608ff6a6bb539592e8
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 23:11:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
